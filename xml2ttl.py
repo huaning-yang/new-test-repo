ad132c6609431cc5e37705657747881dbe9736f0
huaning-yang <huaning.y@gmail.com>
2021-02-25 23:01:50 +0100

    Update xml2ttl.py

--- xml2ttl.py
+++ xml2ttl.py
@@ -81,15 +81,14 @@ def buildGraph(cs):
     g.add((base_url, DCTERMS.creator, Literal("IQB - Institut zur Qualitätsentwicklung im Bildungswesen", lang="de")))
     g.add((base_url, DCTERMS.title, Literal(conceptScheme.label.value, lang=conceptScheme.label.lang )))
     if conceptScheme.definition:
-        for md in metadata:
-            g.add((base_url, DCTERMS.description, Literal(conceptScheme.definition.value + "Folgender Metadatenkatalog wurde verwendet: " + md.cat + " Mit den Definitionen: " + md.d +" Und den Werten: " + md.value, lang=conceptScheme.definition.lang)))
+        g.add((base_url, DCTERMS.description, Literal(conceptScheme.definition.value, lang=conceptScheme.definition.lang)))
         #g.add((base_url, DCTERMS.references, Literal("Folgender Metadatenkatalog wurde verwendet: " + metadata.cat + " Mit den Definitionen: " + metadata.d +" Und den Werten: " + metadata.value)))
         #g.add((base_url, DCTERMS.references, Literal("def " + metadata.d)))
         #g.add((base_url, DCTERMS.references, Literal("value " + metadata.value)))
         for md in metadata:
-            g.add((base_url, SKOS.related, Literal("Folgender Metadatenkatalog wurde verwendet: " + md.cat + " Mit den Definitionen: " + md.d +" Und den Werten: " + md.value)))
+            g.add((base_url, SKOS.related, Literal("Folgender Metadatenkatalog wurde verwendet: " + md.cat + " Mit den Definitionen: " + md.d +" Und den Werten: " + md.value, lang="de")))
         
-        g.add((base_url, SKOS.relatedMatch, Literal("https://huaning-yang.github.io/test-repo-core/index.de.html")))
+        g.add((base_url, SKOS.relatedMatch, Literal("https://huaning-yang.github.io/test-repo-core/index.de.html", lang="de")))
 
     for concept in concepts:
         concept_url = base_url + concept.id
